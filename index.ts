e05554115cacb0b7e75091ff473d4081da5c63c7
Theophil Dio <dev.theophildio@gmail.com>
2025-05-08 21:39:04 +0600

    Problem 3 solved

--- index.ts
+++ index.ts
@@ -2,7 +2,15 @@ function formatString(input: string, toUpper?: boolean): string {
     return toUpper === false ? input.toLocaleLowerCase() : input.toUpperCase();
 }
 
+
 function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
     return items.filter(item => item.rating >= 4);
   }
+
+  
+  function concatenateArrays<T>(...arrays: T[][]): T[] {
+    return arrays.flat();
+  }
+  
+
   
