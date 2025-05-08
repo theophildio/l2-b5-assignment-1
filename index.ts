11259580c1669df0e1cb6be224a639e54f3f177f
Theophil Dio <dev.theophildio@gmail.com>
2025-05-08 21:45:51 +0600

    Problem 4 solved

--- index.ts
+++ index.ts
@@ -13,4 +13,32 @@ function filterByRating(items: { title: string; rating: number }[]): { title: st
   }
   
 
+  class Vehicle {
+    private make: string;
+    private year: number;
+  
+    constructor(make: string, year: number) {
+      this.make = make;
+      this.year = year;
+    }
+  
+    public getInfo(): string {
+      return `Make: ${this.make}, Year: ${this.year}`;
+    }
+  }
+  
+  class Car extends Vehicle {
+    private model: string;
+  
+    constructor(make: string, year: number, model: string) {
+      super(make, year);
+      this.model = model;
+    }
+  
+    public getModel(): string {
+      return `Model: ${this.model}`;
+    }
+  }
+
+  
   
