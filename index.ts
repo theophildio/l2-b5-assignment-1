0c677db304bae7bbcb67ee0e0ecc2ea7027c87b1
Theophil Dio <dev.theophildio@gmail.com>
2025-05-08 21:33:47 +0600

    Problem 2 solved

--- index.ts
+++ index.ts
@@ -2,3 +2,7 @@ function formatString(input: string, toUpper?: boolean): string {
     return toUpper === false ? input.toLocaleLowerCase() : input.toUpperCase();
 }
 
+function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
+    return items.filter(item => item.rating >= 4);
+  }
+  
